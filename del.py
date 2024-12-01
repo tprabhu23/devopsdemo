cb77ba19a53663cf7252e6c848dec3500358169d
Prabhu Thangaraj-EXT <prabhu.thangaraj-ext@vsei.com>
2024-12-01 11:50:02 -0500

    del commit

---
+++ del.py
@@ -0,0 +1,11 @@
+print ("------------next program -5 --------------")
+
+# using 'del' variable. In python possible to delete the variable
+
+a=122
+print (a)
+#below deleting variable using 'del' keyword
+del a
+#print (a)
+# result will show the error here, so i comment the above line again
+
