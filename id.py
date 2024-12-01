3a71e4291c20a85f26fa8b4bad545992594c3f49
Prabhu Thangaraj-EXT <prabhu.thangaraj-ext@vsei.com>
2024-12-01 09:40:53 -0500

    second commit:q!

--- id.py
+++ id.py
@@ -5,4 +5,23 @@ print(id(a))
 print(id(b))
 #below i'm reassined the variable 'b' , here it will chage the id value
 b=44
-print (id(b))
+print (id(b)
+
+#--------------------
+
+
+print ("------------next program -2 -------------")
+#multiple assignment in Python
+# the same value that can be assign multiple variables
+# below assigned the same value to all variales
+a=b=c=50
+print(a)
+print(b)
+print(c)
+
+#also print the id value for a b c
+
+print(id(a))
+print(id(b))
+print(id(c))
+)
